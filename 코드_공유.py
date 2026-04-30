4506ed7fc9d902af19b8d8510407464fe280e0bc
김연수 <yeonsoo@gim-yeonsuui-MacBookPro.local>
2026-04-30 16:22:42 +0900

    Add DT model

--- 코드_공유.py
+++ 코드_공유.py
@@ -16,12 +16,46 @@ wine = load_wine()
 # target은 'target' 컬럼만을 대상으로 합니다.
 # X, y 데이터를 test size는 0.2, random_state 값은 42로 하여 train 데이터와 test 데이터로 분할합니다.
 
-''' 코드 작성 바랍니다 '''
 
+df = pd.DataFrame(wine.data, columns=wine.feature_names)
+df['target'] = wine.target
+
+
+X = df.drop('target', axis=1)
+y = df['target']
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=42
+)
 
 ####### A 작업자 작업 수행 #######
 
-''' 코드 작성 바랍니다 '''
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+dt_param_grid = {
+    'max_depth': [3, 4, 5, 6],
+    'min_samples_split': [2, 3, 4]
+}
+
+dt_grid = GridSearchCV(
+    DecisionTreeClassifier(random_state=42),
+    param_grid=dt_param_grid,
+    cv=5
+)
+
+dt_grid.fit(X_train, y_train)
+
+dt_best_model = dt_grid.best_estimator_
+dt_pred = dt_best_model.predict(X_test)
+
+dt_accuracy = accuracy_score(y_test, dt_pred)
+
+print("DT Best Params:", dt_grid.best_params_)
+print("DT Accuracy:", dt_accuracy)
 
 
 
