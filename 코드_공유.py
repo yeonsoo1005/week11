95e5b93bb764e686c54fe18a5af5cdcc18be3dc4
김연수 <yeonsoo@gim-yeonsuui-MacBookPro.local>
2026-04-30 16:25:19 +0900

    Add XGB model

--- 코드_공유.py
+++ 코드_공유.py
@@ -16,8 +16,18 @@ wine = load_wine()
 # target은 'target' 컬럼만을 대상으로 합니다.
 # X, y 데이터를 test size는 0.2, random_state 값은 42로 하여 train 데이터와 test 데이터로 분할합니다.
 
-''' 코드 작성 바랍니다 '''
 
+df = pd.DataFrame(wine.data, columns=wine.feature_names)
+df['target'] = wine.target
+
+X = df.drop('target', axis=1)
+y = df['target']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=42
+)
 
 ####### A 작업자 작업 수행 #######
 
@@ -27,5 +37,33 @@ wine = load_wine()
 
 ####### B 작업자 작업 수행 #######
 
-''' 코드 작성 바랍니다 '''
 
+####### B 작업자 작업 수행 #######
+
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score
+
+xgb_param_grid = {
+    'max_depth': [3, 4, 5],
+    'learning_rate': [0.01, 0.1],
+    'n_estimators': [50, 100]
+}
+
+xgb_grid = GridSearchCV(
+    XGBClassifier(
+        random_state=42,
+        eval_metric='mlogloss'
+    ),
+    param_grid=xgb_param_grid,
+    cv=5
+)
+
+xgb_grid.fit(X_train, y_train)
+
+xgb_best_model = xgb_grid.best_estimator_
+xgb_pred = xgb_best_model.predict(X_test)
+
+xgb_accuracy = accuracy_score(y_test, xgb_pred)
+
+print("XGB Best Params:", xgb_grid.best_params_)
+print("XGB Accuracy:", xgb_accuracy)
